fb101c4a5814f0077cbdff5d46a67b5c61526735
shicks <shicks255@yahoo.com>
2018-08-12 19:52:03 -0400

    a project euler refactor

--- src/main/java/ProjectEuler/Euler3.java
+++ src/main/java/ProjectEuler/Euler3.java
@@ -1,26 +1,69 @@
 package ProjectEuler;
 
-
-import java.util.Scanner;
-
 public class Euler3
 {
     public static void main(String[] args)
     {
-        Scanner key = new Scanner(System.in);
         long number = 600851475143L;
-        for (long i = 1; i < number; i++)
-        {
-            long a = number % i;
+        System.out.println("answer is " + calculate2(number));
+    }
 
-            if (a == 0 && isPrime(i))
+    public static long calculate2(Long number)
+    {
+        long factor = 2;
+        long lastFactor = 1;
+        while (number > 1)
+        {
+            if (number % factor == 0)
             {
-                System.out.println(i);
+                lastFactor = factor;
+                number = number / factor;
+                while (number % factor == 0)
+                {
+                    number = number / factor;
+                }
             }
+            factor = factor+1;
         }
+
+        return lastFactor;
+    }
+
+    public static long calculate(Long number)
+    {
+        long courser = number-1;
+        long answer = 0;
+
+        while (courser > 1)
+        {
+            System.out.println(courser);
+            if (number % courser == 0)
+                if (isPrime2(courser))
+                    if (courser > answer)
+                        answer = courser;
+
+            courser--;
+        }
+
+        return answer;
     }
 
     public static boolean isPrime(Long  a)
+    {
+        long x = 2;
+        long target = a/2;
+        while (x < target)
+        {
+            if (a % x == 0)
+                return false;
+
+            x++;
+        }
+
+        return true;
+    }
+
+    public static boolean isPrime2(Long  a)
     {
         boolean prime = false;
         int count = 0;
