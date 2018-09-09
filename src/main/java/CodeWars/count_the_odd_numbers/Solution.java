1879f4d35b0e5efaf014fb7e6a32fad4d1fd6e76
shicks <shicks255@yahoo.com>
2018-09-09 17:31:08 -0400

    trying this more

--- src/main/java/CodeWars/count_the_odd_numbers/Solution.java
+++ src/main/java/CodeWars/count_the_odd_numbers/Solution.java
@@ -15,21 +15,39 @@
 
 package CodeWars.count_the_odd_numbers;
 
-import
-
 public class Solution
 {
-    private final static int[] TRIANGLE = new int[]{1,3,5,7,9,11,13,15,17,19,21,23,25,27,29};
+    private final static int[] TRIANGLE = new int[]{1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,
+    51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99,101,103,105,107,109,111,113,115,117,119,
+    121,123,125,127,129,131,133,135,137,139};
 
     public static void main(String[] args)
     {
-        System.out.println(rowSumOddNumbers(1));
-        System.out.println(rowSumOddNumbers(2));
+//        System.out.println(rowSumOddNumbers(1));
+//        System.out.println(rowSumOddNumbers(2));
+//        System.out.println(rowSumOddNumbers(3));
+//        System.out.println(rowSumOddNumbers(4));
+        System.out.println(rowSumOddNumbers(8));
     }
 
-    public static int rowSumOddNumbers(int i)
+    public static int rowSumOddNumbers(int n)
     {
+        //need to make sure
 
+
+        int startAtIndex = 0;
+
+        for (int i = n-1; i > 0; i--)
+            startAtIndex += i;
+
+        if ((startAtIndex + n) > TRIANGLE.length)
+            return 0;
+
+        int answer = 0;
+        for (int i = startAtIndex; i < startAtIndex+n; i++)
+            answer += TRIANGLE[i];
+
+        return answer;
     }
 
 }
