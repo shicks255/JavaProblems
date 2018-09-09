64effee90a729ad074615a455cb2132660ba5911
shicks <shicks255@yahoo.com>
2018-09-09 19:59:13 -0400

    finally got this one going

--- src/main/java/CodeWars/count_the_odd_numbers/Solution.java
+++ src/main/java/CodeWars/count_the_odd_numbers/Solution.java
@@ -2,11 +2,11 @@
 
 //Given the triangle of consecutive odd numbers:
 //
-//                 1
-//              3     5
-//           7     9    11
-//        13    15    17    19
-//     21    23    25    27    29
+//                 1                 1
+//              3     5              2
+//           7     9    11           3
+//        13    15    17    19       4
+//     21    23    25    27    29    5
 //        ...
 //        Calculate the row sums of this triangle from the row index (starting at index 1) e.g.:
 //
@@ -17,35 +17,19 @@ package CodeWars.count_the_odd_numbers;
 
 public class Solution
 {
-    private final static int[] TRIANGLE = new int[]{1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,
-    51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99,101,103,105,107,109,111,113,115,117,119,
-    121,123,125,127,129,131,133,135,137,139};
-
     public static void main(String[] args)
     {
-//        System.out.println(rowSumOddNumbers(1));
-//        System.out.println(rowSumOddNumbers(2));
-//        System.out.println(rowSumOddNumbers(3));
-//        System.out.println(rowSumOddNumbers(4));
-        System.out.println(rowSumOddNumbers(8));
+        System.out.println(rowSumOddNumber(4));
     }
 
-    public static int rowSumOddNumbers(int n)
+    public static int rowSumOddNumber(int n)
     {
-        //need to make sure
+        int numberToStart = n * (n-1) + 1;
 
+        int answer = numberToStart;
 
-        int startAtIndex = 0;
-
-        for (int i = n-1; i > 0; i--)
-            startAtIndex += i;
-
-        if ((startAtIndex + n) > TRIANGLE.length)
-            return 0;
-
-        int answer = 0;
-        for (int i = startAtIndex; i < startAtIndex+n; i++)
-            answer += TRIANGLE[i];
+        for (int i = 1; i < n; i++)
+            answer += (numberToStart += 2);
 
         return answer;
     }
