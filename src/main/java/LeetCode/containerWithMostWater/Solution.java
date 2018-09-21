b4604b8a352add2b46f1e76cd9f3e23f945004d5
shicks <shicks255@yahoo.com>
2018-09-20 22:32:47 -0400

    new leetcode problem, first solved on whiteboard and the solution was correc.

---
+++ src/main/java/LeetCode/containerWithMostWater/Solution.java
@@ -0,0 +1,49 @@
+//https://leetcode.com/problems/container-with-most-water/description/
+
+/**
+ * Given n non-negative integers a1, a2, ..., an , where each represents a point at
+ * coordinate (i, ai). n vertical lines are drawn such that the two endpoints
+ * of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container,
+ * such that the container contains the most water.
+ */
+
+package LeetCode.containerWithMostWater;
+
+public class Solution
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println(mostWater(new int[]{1,8,6,2,5,4,8,3,7}));
+    }
+
+    public static int mostWater(int[] coords)
+    {
+        int biggest = 0;
+        int windowSize = coords.length;
+
+        while (windowSize > 1)
+        {
+            int panes = (coords.length - windowSize) + 1;
+
+            for (int i = 0; i < panes; i++)
+            {
+                int leftIndex = i;
+                int rightIndex = i + (windowSize-1);
+
+                int left = coords[leftIndex];
+                int right = coords[rightIndex];
+
+                int amount = (windowSize-1) * (left > right ? right : left);
+                if (amount > biggest)
+                    biggest = amount;
+            }
+
+            windowSize--;
+        }
+
+        return biggest;
+    }
+
+
+}
