f0de5ce63ed4483db96a108d30dcd4e97cc9a7ac
shicks <shicks255@yahoo.com>
2018-09-23 12:37:52 -0400

    more optimized selection sort

---
+++ src/main/java/SearchingAndSorting/QuickSort.java
@@ -0,0 +1,39 @@
+package SearchingAndSorting;
+
+import java.util.Arrays;
+
+public class QuickSort
+{
+
+    public static void main(String[] args)
+    {
+        QuickSort quickSort = new QuickSort();
+
+        System.out.println(Arrays.toString(quickSort.sort(new int[]{5,6,7,4,8,2,9,1})));
+
+    }
+
+    public int[] sort(int[] array)
+    {
+//        if (array.length == 2)
+//        {
+//            if (array[0] > array[1])
+//            {
+//                int temp = array[1];
+//                array[0] = array[1];
+//                array[1] = temp;
+//            }
+//        }
+
+        if (array.length > 1)
+        {
+            int pivot = array.length / 2;
+
+            int[] smaller
+        }
+
+        return array;
+    }
+
+}
+
