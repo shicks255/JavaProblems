5e4450e088dc7dab17012d17a261e5b332706aac
shicks <shicks255@yahoo.com>
2018-09-22 21:42:00 -0400

    more optimized selection sort

--- src/main/java/SearchingAndSorting/SelectionSort.java
+++ src/main/java/SearchingAndSorting/SelectionSort.java
@@ -7,30 +7,24 @@ public class SelectionSort
     public static void main(String[] args)
     {
         SelectionSort selectionSort = new SelectionSort();
-        System.out.println(Arrays.toString(selectionSort.sort(new int[]{5,5,4,3,2,1})));
     }
 
     public int[] sort(int[] array)
     {
         for (int i = 0; i < array.length; i++)
         {
-            int min = array[i];
-            int swapIndex = 0;
+            int min = i;
 
             for (int j = i+1; j < array.length; j++)
-                if (array[j] < min)
-                {
-                    min = array[j];
-                    swapIndex = j;
-                }
+                if (array[j] < array[min])
+                    min = j;
 
-            if (min != array[i])
+            if (min != i)
             {
                 int temp = array[i];
-                array[i] = min;
-                array[swapIndex] = temp;
+                array[i] = array[min];
+                array[min] = temp;
             }
-
         }
 
         return array;
