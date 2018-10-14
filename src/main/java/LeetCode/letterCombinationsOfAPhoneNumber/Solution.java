9c94bf8a388635783dcb19fefebb99b3fc4a340d
shicks <shicks255@yahoo.com>
2018-10-14 18:48:33 -0400

    finally getting closer here

--- src/main/java/LeetCode/letterCombinationsOfAPhoneNumber/Solution.java
+++ src/main/java/LeetCode/letterCombinationsOfAPhoneNumber/Solution.java
@@ -6,12 +6,15 @@ public class Solution
 {
     public static void main(String[] args)
     {
-        System.out.println(letterCombinations("234"));
+        System.out.println(letterCombinations("5678"));
         //["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
     }
 
     public static List<String> letterCombinations(String digits)
     {
+        if (digits.length()==0)
+            return Collections.emptyList();
+
         Map<String, List<String>> combos = new HashMap<>();
         combos.put("2", Arrays.asList("a", "b", "c"));
         combos.put("3", Arrays.asList("d", "e", "f"));
@@ -21,27 +24,23 @@ public class Solution
         combos.put("7", Arrays.asList("p", "q", "r", "s"));
         combos.put("8", Arrays.asList("t", "u", "v"));
         combos.put("9", Arrays.asList("w", "x", "y", "z"));
+        if (digits.length() == 1)
+            return combos.get(digits);
 
         String[] phoneNumbers = digits.split("");
-
-        List<List<String>> sequences = new ArrayList<>();
+        List<String> solution = new ArrayList<>();
 
         int counter = 1;
         for (String digit : phoneNumbers)
         {
             List<String> possibleDigits = combos.get(digit);
-            sequences.add(possibleDigits);
             counter *= possibleDigits.size();
         }
 
-        List<String> solution = new ArrayList<>();
-//        Arrays.fill(solution, "");
-
-        int counterIncrementer = phoneNumbers.length-1;
-        int lastTouched = counterIncrementer;
         int[] counters = new int[phoneNumbers.length];
-
         Arrays.fill(counters, 0);
+
+        int lastTouched = counters.length-2;
         for (int i = 0; i < counter; i++) //iterate over the amount of spaces were going to need
         {
             String solutionString = "";
@@ -49,27 +48,22 @@ public class Solution
             for (int j = 0; j < counters.length; j++)
             {
                 solutionString += combos.get(phoneNumbers[j]).get(counters[j]);
-                if (counterIncrementer == j)
-                    counters[j]++;
-
-                if (combos.get(phoneNumbers[j]).size() == counters[j])
+                if (j == counters.length-1) // last counter
                 {
-                    //the counterIncrementer needs to be flipped, and CI goes up one.  Everything below it goes to 0 as well
-                    if (combos.get(phoneNumbers[lastTouched]).size() <= counters[lastTouched])
-                    {
-                        for (int k = counterIncrementer; k < counters.length; k++)
-                        {
-                            counters[k] = 0;
-                            lastTouched--;
-                        }
-                        counters[lastTouched]++;
-                    }
-                    else
+                    counters[j]++;
+                    if (combos.get(phoneNumbers[j]).size() <= counters[j]) //
                     {
                         counters[lastTouched]++;
-                        for (int k = lastTouched+1; k < counters.length; k++)
+                        counters[j] = 0;
+                        if (combos.get(phoneNumbers[lastTouched]).size() <= counters[lastTouched])
                         {
-                            counters[k] = 0;
+                            if (lastTouched > 0)
+                            {
+                                counters[lastTouched-1]++;
+                                for (int k = lastTouched; k < counters.length; k++)
+                                    counters[k] = 0;
+                                lastTouched = counters.length-2;
+                            }
                         }
                     }
                 }
