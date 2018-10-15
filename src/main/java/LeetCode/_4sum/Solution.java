4955e1e810d92d93db1f2d0bebc5e6b5cab67e69
shicks <shicks255@yahoo.com>
2018-10-15 15:03:44 -0400

    setting up new problem

---
+++ src/main/java/LeetCode/_4sum/Solution.java
@@ -0,0 +1,38 @@
+//https://leetcode.com/problems/4sum/description/
+
+/**Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target?
+        Find all unique quadruplets in the array which gives the sum of target.
+
+        Note:
+        The solution set must not contain duplicate quadruplets.
+
+        Example:
+        Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+        A solution set is:
+        [
+            [-1,  0, 0, 1],
+            [-2, -1, 1, 2],
+            [-2,  0, 0, 2]
+        ]
+*/
+
+package LeetCode._4sum;
+
+import java.util.List;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public List<List<Integer>> fourSum(int[] nums, int target)
+    {
+
+
+        return null;
+    }
+
+
+}
