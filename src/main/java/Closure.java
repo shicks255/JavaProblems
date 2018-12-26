fb97b5dd21f0ca7bc0df099f90736d12bac2a076
shicks <shicks255@yahoo.com>
2018-12-26 18:23:55 -0500

    playing around with high order functions

--- src/main/java/Closure.java
+++ src/main/java/Closure.java
@@ -1,12 +1,20 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Closure
 {
 
     public static void main(String[] args)
+    {
+//        simpleClosure();
+        useFunctions();
+    }
+
+    public static void simpleClosure()
     {
         List<String> titles = new ArrayList<>(Arrays.asList("king robert", "queen mary", "prince william", "prince henry"));
 
@@ -16,11 +24,37 @@ public class Closure
 //        titles.removeIf(isKing);
 //        titles.removeIf(isQueen);
         //or
-        titles.removeIf(startsWith("king"));
-        titles.removeIf(startsWith("queen"));
-
-
+        System.out.println(titles.stream().filter(startsWith("king")).collect(Collectors.joining()));
+        System.out.println(titles.stream().filter(startsWith("queen")).collect(Collectors.joining()));
         System.out.println(titles);
+    }
+
+    public static void useFunctions()
+    {
+        Function<String, Predicate<String>> makeNameChecker =
+                (String nameToCheck) ->
+                        (String check) -> check.equalsIgnoreCase(nameToCheck);
+
+        List<String> names = new ArrayList<>(Arrays.asList("Joey", "Steve", "steven", "eric", "chris", "ed", "sean", "david", "alex"));
+
+        List<String> onlyErics = names.stream()
+                .filter(makeNameChecker.apply("eric"))
+                .collect(Collectors.toList());
+        System.out.println(onlyErics);
+
+        List<String> onlySteve = names.stream()
+                .filter(makeNameChecker.apply("steve"))
+                .collect(Collectors.toList());
+        System.out.println(onlySteve);
+
+        //or to be even more concise...
+        Function<String, Predicate<String>> makeNameChecker2 =
+                nameToCheck -> checkThisName -> checkThisName.equalsIgnoreCase(nameToCheck);
+
+        List<String> onlyDave = names.stream()
+                .filter(makeNameChecker2.apply("david"))
+                .collect(Collectors.toList());
+        System.out.println(onlyDave);
     }
 
     public static Predicate<String> startsWith(String letter)
@@ -29,16 +63,16 @@ public class Closure
     }
 
     //old way, none lambda
-//    public static Predicate<String> startsWith(String letter)
-//    {
-//        return new Predicate<String>()
-//        {
-//            @Override
-//            public boolean test(String s)
-//            {
-//                return s.startsWith(letter);
-//            }
-//        };
-//    }
+    public static Predicate<String> startsWithOld(String letter)
+    {
+        return new Predicate<String>()
+        {
+            @Override
+            public boolean test(String s)
+            {
+                return s.startsWith(letter);
+            }
+        };
+    }
 
 }
