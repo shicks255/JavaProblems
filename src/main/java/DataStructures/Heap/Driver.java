77db278d9a8c3c4513154b4afe1e09e0752ba4dc
shicks <shicks255@yahoo.com>
2019-05-02 21:24:04 -0400

    making the min heap

--- src/main/java/DataStructures/Heap/Driver.java
+++ src/main/java/DataStructures/Heap/Driver.java
@@ -2,8 +2,33 @@ package DataStructures.Heap;
 
 public class Driver
 {
-
     public static void main(String[] args)
+    {
+//        testMaxHeap();
+        testMinHeap();
+    }
+
+    public static void testMinHeap()
+    {
+        Heap.MinHeap heap = new Heap.MinHeap();
+        heap.add(10);
+        heap.add(9);
+        heap.add(0);
+        heap.add(7);
+        heap.add(4);
+        heap.add(5);
+        heap.add(2);
+        heap.add(1);
+        heap.add(0);
+        heap.add(40);
+        heap.add(12);
+
+        System.out.println(heap.toString());
+        heap.pop();
+        System.out.println(heap.toString());
+    }
+
+    public static void testMaxHeap()
     {
         Heap.MaxHeap heap = new Heap.MaxHeap();
         heap.add(0);
--- src/main/java/DataStructures/Heap/Heap.java
+++ src/main/java/DataStructures/Heap/Heap.java
@@ -98,7 +98,94 @@ public class Heap
 
     public static class MinHeap
     {
+        int[] heap = new int[10];
+        int capacity = 0;
 
+        @Override
+        public String toString()
+        {
+            return Arrays.toString(heap);
+        }
+
+        public void add(int item)
+        {
+            if (isFull())
+                increaseSize();
+            heap[capacity] = item;
+            capacity++;
+            heapifyUp();
+        }
+
+        public boolean isFull()
+        {
+            return heap.length == capacity;
+        }
+
+        public void increaseSize()
+        {
+            heap = Arrays.copyOf(heap, heap.length * 2);
+        }
+
+        public int getParentIndex(int index)
+        {
+            if (index % 2 == 0)
+                return (index / 2)-1;
+            else
+                return index/2;
+        }
+
+        public int getLeftChildIndex(int index)
+        {
+            return (index * 2)-1;
+        }
+
+        public int getRightChildIndex(int index)
+        {
+            return (index*2)-2;
+        }
+
+        public int pop()
+        {
+            int temp = heap[0];
+
+            heap[0] = heap[capacity-1];
+            heap[capacity-1] = 0;
+            capacity--;
+            heapifyDown();
+            return temp;
+        }
+
+        public void heapifyUp()
+        {
+            int indexToTest = capacity-1;
+
+            while (indexToTest > 0)
+            {
+                if (heap[indexToTest] < heap[getParentIndex(indexToTest)])
+                {
+                    int temp = heap[indexToTest];
+                    heap[indexToTest] = heap[getParentIndex(indexToTest)];
+                    heap[getParentIndex(indexToTest)] = temp;
+                }
+                indexToTest--;
+            }
+        }
+
+        public void heapifyDown()
+        {
+            int indexToTest = 1;
+
+            while (indexToTest < capacity)
+            {
+                if (heap[indexToTest] < heap[getParentIndex(indexToTest)])
+                {
+                    int temp = heap[indexToTest];
+                    heap[indexToTest] = heap[getParentIndex(indexToTest)];
+                    heap[getParentIndex(indexToTest)] = temp;
+                }
+                indexToTest++;
+            }
+        }
     }
 
 }
