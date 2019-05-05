f2daf1f23bba389f578f2934ab5d98344a39e0d2
shicks <shicks255@yahoo.com>
2019-05-05 16:54:12 -0400

    finally got an optimized solution

--- src/main/java/LeetCode/SubstringWithConcatenationOfAllWords/Solution.java
+++ src/main/java/LeetCode/SubstringWithConcatenationOfAllWords/Solution.java
@@ -1,22 +1,61 @@
 package LeetCode.SubstringWithConcatenationOfAllWords;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Solution
 {
 
     public static void main(String[] args)
     {
-        System.out.println(findSubstring2("abcdefghi", new String[]{"abc", "def"}));
-        System.out.println(findSubstring2("barfoothefoobarman", new String[]{"foo", "bar"}));
-        System.out.println(findSubstring2("wordgoodgoodgoodbestword", new String[]{"word", "good", "best", "word"}));
+        System.out.println(findSubstring3("abcdefghi", new String[]{"abc", "def"}));
+        System.out.println(findSubstring3("barfoothefoobarman", new String[]{"foo", "bar"}));
+        System.out.println(findSubstring3("wordgoodgoodgoodbestword", new String[]{"word", "good", "best", "word"}));
 //        System.out.println(findSubstring("steve", new String[]{}));
-        System.out.println(findSubstring2("lingmindraboofooowingdingbarrwingmonkeypoundcake",
+        System.out.println(findSubstring3("lingmindraboofooowingdingbarrwingmonkeypoundcake",
                 new String[]{"fooo","barr","wing","ding","wing"}));
-        System.out.println(findSubstring2("abcdefghi", new String[]{"def", "ghi"}));
+//        System.out.println(findSubstring2("abcdefghi", new String[]{"def", "ghi"}));
+    }
+
+    public static List<Integer> findSubstring3(String s, String[] words)
+    {
+        List<Integer> answer = new ArrayList<>();
+        if (words.length == 0)
+            return answer;
+
+        Map<String, Integer> wordFrequencyMap = new HashMap<>();
+        for (String word : words)
+        {
+            Integer count = wordFrequencyMap.getOrDefault(word, 0);
+            wordFrequencyMap.put(word, ++count);
+        }
+
+        int wordCount = words.length;
+        int wordLength = words[0].length();
+        int stride = wordCount*wordLength;
+
+        for (int i = 0; i < s.length()-stride+1; i++)
+        {
+            Map<String, Integer> wfm = new HashMap<>(wordFrequencyMap);
+            int startingPoint = i;
+            for (int j = 0; j < wordCount; j++)
+            {
+                String chunk = s.substring(startingPoint, startingPoint+wordLength);
+
+                if (wfm.containsKey(chunk))
+                {
+                    Integer frequency = wfm.get(chunk);
+                    frequency -= 1;
+                    wfm.put(chunk, frequency);
+                }
+
+                if (wfm.values().stream().allMatch(x -> x == 0))
+                    answer.add(i);
+
+                startingPoint += wordLength;
+            }
+        }
+
+        return answer;
     }
 
     public static List<Integer> findSubstring2(String s, String[] words)
