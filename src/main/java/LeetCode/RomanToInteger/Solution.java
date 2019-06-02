c08b3008968ad09e2c09538a24e07b048b9b6621
shicks <shicks255@yahoo.com>
2019-06-02 11:40:38 -0400

    romanToInt leetcode

--- src/main/java/LeetCode/RomanToInteger/Solution.java
+++ src/main/java/LeetCode/RomanToInteger/Solution.java
@@ -5,13 +5,16 @@ import java.util.Map;
 
 public class Solution
 {
-
     public static void main(String[] args)
     {
-
+//        System.out.println(romanToInt("III"));
+//        System.out.println(romanToInt("IV"));
+//        System.out.println(romanToInt("IX"));
+        System.out.println(romanToInt("LVIII"));
+        System.out.println(romanToInt("MCMXCIV"));
     }
 
-    public int romanToInt(String s)
+    public static int romanToInt(String s)
     {
         Map<String, Integer> numerals = new HashMap<>();
         numerals.put("I", 1);
@@ -23,11 +26,38 @@ public class Solution
         numerals.put("M", 1000);
 
         int answer = romanToInt(s, numerals);
+        return answer;
     }
 
-    public int romanToInt(String s, Map<String, Integer> numerals)
+    public static int romanToInt(String s, Map<String, Integer> numerals)
     {
+        if (s == null || s.length() < 1)
+            return 0;
 
+        if (s.length() == 1)
+            return numerals.get(s);
+
+        String first = s.substring(0,1);
+        int amount = numerals.get(first);
+
+        String second = s.substring(1,2);
+        if (first.equals("I") && (second.equals("V") || second.equals("X")))
+        {
+            amount = numerals.get(second) - amount;
+            return amount + romanToInt(s.substring(2), numerals);
+        }
+        if (first.equals("X") && (second.equals("L") || second.equals("C")))
+        {
+            amount = numerals.get(second) - amount;
+            return amount + romanToInt(s.substring(2), numerals);
+        }
+        if (first.equals("C") && (second.equals("D") || second.equals("M")))
+        {
+            amount = numerals.get(second) - amount;
+            return amount + romanToInt(s.substring(2), numerals);
+        }
+
+        return amount + romanToInt(s.substring(1), numerals);
     }
 
 }
