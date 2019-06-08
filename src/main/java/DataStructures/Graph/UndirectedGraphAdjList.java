bc752bb07e981c4610290d64ecf963b5b1e33fa9
shicks <shicks255@yahoo.com>
2019-06-07 23:59:51 -0400

    graphs

---
+++ src/main/java/DataStructures/Graph/UndirectedGraphAdjList.java
@@ -0,0 +1,71 @@
+package DataStructures.Graph;
+
+import java.util.*;
+
+public class UndirectedGraphAdjList
+{
+    private int V; //number of vertices
+    private int E; //number of edges
+    private Set<Integer>[] adj;
+
+    public UndirectedGraphAdjList(int V)
+    {
+        this.V = V;
+        this.E = 0;
+        adj = new TreeSet[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new TreeSet<>();
+    }
+
+    public void addEdge(int src, int dest)
+    {
+        E++;
+        adj[src].add(dest);
+        adj[dest].add(src);
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder str = new StringBuilder();
+        for (int v = 0; v < this.V; v++)
+        {
+            str.append("\r\nList of vertexes for vertex " + v + ": ");
+            str.append(v + " -> ");
+            for (Integer e : this.adj[v])
+                str.append(e + " -> ");
+        }
+
+        return str.toString();
+    }
+
+    public int breadthFirstSet()
+    {
+        Deque<Integer> queue = new ArrayDeque<>();
+
+
+
+    }
+
+
+    public static void main(String[] args)
+    {
+        UndirectedGraphAdjList graph = new UndirectedGraphAdjList(5);
+        graph.addEdge(0, 1);
+        graph.addEdge(0,4);
+        graph.addEdge(1,4);
+        graph.addEdge(1,3);
+        graph.addEdge(1,2);
+        graph.addEdge(2,1);
+        graph.addEdge(2,3);
+        graph.addEdge(3,1);
+        graph.addEdge(3,2);
+        graph.addEdge(3,4);
+        graph.addEdge(4,0);
+        graph.addEdge(4,1);
+        graph.addEdge(4,3);
+
+        System.out.println(graph);
+    }
+
+}
---
+++ src/main/java/DataStructures/Graph/UndirectedGraphMatrix.java
@@ -0,0 +1,17 @@
+package DataStructures.Graph;
+
+public class UndirectedGraphMatrix
+{
+    private int[][] adj;
+
+
+    public UndirectedGraphMatrix()
+    {
+        this.adj = new int[][]{};
+
+    }
+
+
+
+
+}
